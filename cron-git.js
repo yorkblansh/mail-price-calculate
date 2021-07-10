08682d10ad53a0666a2ce492aac97bddcd880761
york blansh <yorkblansh@gmail.com>
2021-07-10 20:43:01 +0300

    Add CronJob package

--- cron-git.js
+++ cron-git.js
@@ -2,13 +2,13 @@ const app = require('express')();
 const git = require('simple-git')();
 const CronJob = require('cron').CronJob; 
 
-
+console.log(git().status()); 
 
 console.log('Before job instantiation'); 
 
-const job = new CronJob('0 */3 * * * *', function() { 
+const job = new CronJob('0 */1 * * * *', function() { 
 
-git.status();
+console.log(git.status());
 
 });
 console.log('After job instantiation'); 
