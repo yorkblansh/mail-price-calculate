96e44f1904f5c6f3c2d1b3c8d1195d2a723b64b9
york blansh <yorkblansh@gmail.com>
2021-07-10 21:15:41 +0300

    Refactor & Rename cron file (updating from git)

--- cron-git_mail_calculate.js
+++ cron-git_mail_calculate.js
@@ -2,14 +2,16 @@ const app = require('express')();
 const git = require('simple-git')();
 const CronJob = require('cron').CronJob; 
 
-console.log(git().status()); 
+ 
 
 console.log('Before job instantiation'); 
 
 const job = new CronJob('0 */1 * * * *', function() { 
 
-console.log(git.status());
+// results in 'git pull origin master --no-rebase'
+    git.pull('origin', 'master', {'--no-rebase': null})
 
 });
 console.log('After job instantiation'); 
 job.start(); 
+
